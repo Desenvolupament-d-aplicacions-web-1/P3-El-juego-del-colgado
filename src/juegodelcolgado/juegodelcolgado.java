48e46020f59fc489481962e6f6e16fd4faf24a48
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2024-11-26 16:25:33 +0100

    UPDATE: juegodelcolgado.java
    Creacion del Scanner

--- src/juegodelcolgado/juegodelcolgado.java
+++ src/juegodelcolgado/juegodelcolgado.java
@@ -1,10 +1,14 @@
 package juegodelcolgado;
 
+import java.util.Scanner;
+
 public class juegodelcolgado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner s = new Scanner(System.in);
+		
 	}
 
 }
