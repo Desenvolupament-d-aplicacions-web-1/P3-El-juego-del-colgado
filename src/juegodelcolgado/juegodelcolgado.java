e8b4ccaddd96bdbcc63df7c0e9e27e5df60dd7a0
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2024-12-01 18:22:10 +0100

    Creo el scanner y pregunto cuantos jugadores quieren jugar
    En caso de que el numero sea menor que 2 dara error

--- src/juegodelcolgado/juegodelcolgado.java
+++ src/juegodelcolgado/juegodelcolgado.java
@@ -7,7 +7,19 @@ public class juegodelcolgado {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// Creo el scanner
 		Scanner s = new Scanner(System.in);
+		
+		System.out.println("¡Bienvenidos al juego del Colgado!");
+		
+		// Pido cuantos jugadores quieren jugar
+		System.out.print("¿Cuántos jugadores quieren jugar? (mínimo 2): ");
+        int numJugadores = s.nextInt();
+        
+        if (numJugadores < 2) {
+            System.out.println("Debe haber al menos 2 jugadores para jugar.");
+            return;
+        }
 		
 	}
 
